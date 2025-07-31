e470962b4ca51b0822410a9e8a215826f1bf853a
amirmohammad2001 <amirmohammad.saleh2001@gmail.com>
2025-08-01 01:02:07 +0330

    gradle setting

---
+++ src/main/java/org/example/hw3/Hw3Application.java
@@ -0,0 +1,13 @@
+package org.example.hw3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw3Application.class, args);
+    }
+
+}
